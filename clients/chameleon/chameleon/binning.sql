9dad629f44b18c32e0721886b9c20abc66bbad65
Arindam Saha <arindamsaha1507@gmail.com>
2025-02-26 13:07:39 +0000

    Templated sql query

--- clients/chameleon/chameleon/binning.sql
+++ clients/chameleon/chameleon/binning.sql
@@ -1,41 +1,41 @@
 COPY (
     WITH power_data AS (
-        SELECT time_bucket(INTERVAL '30 minutes', reading_timestamp) AS bucket,
+        SELECT time_bucket(INTERVAL '{interval}', reading_timestamp) AS bucket,
             cad_id,
             ROUND(AVG(reading), 3) AS power_avg,
             COUNT(*) AS power_event_count,
             MIN(reading) AS power_min,
             MAX(reading) AS power_max
         FROM power_events
-        WHERE reading_timestamp >= $start_time
-            AND reading_timestamp < $end_time
+        WHERE reading_timestamp >= '{start_time}'
+            AND reading_timestamp < '{end_time}'
             AND commodity = 'elec'
         GROUP BY bucket,
             cad_id
     ),
     temperature_data AS (
-        SELECT time_bucket(INTERVAL '5 minutes', meter_update_timestamp) AS bucket,
+        SELECT time_bucket(INTERVAL '{interval}', meter_update_timestamp) AS bucket,
             cad_id,
             ROUND(AVG(reading), 3) AS temperature_avg,
             COUNT(*) AS temperature_event_count,
             MIN(reading) AS temperature_min,
             MAX(reading) AS temperature_max
         FROM temperature_events
-        WHERE meter_update_timestamp >= $start_time
-            AND meter_update_timestamp < $end_time
+        WHERE meter_update_timestamp >= '{start_time}'
+            AND meter_update_timestamp < '{end_time}'
         GROUP BY bucket,
             cad_id
     ),
     humidity_data AS (
-        SELECT time_bucket(INTERVAL '5 minutes', meter_update_timestamp) AS bucket,
+        SELECT time_bucket(INTERVAL '{interval}', meter_update_timestamp) AS bucket,
             cad_id,
             ROUND(AVG(reading), 3) AS humidity_avg,
             COUNT(*) AS humidity_event_count,
             MIN(reading) AS humidity_min,
             MAX(reading) AS humidity_max
         FROM humidity_events
-        WHERE meter_update_timestamp >= $start_time
-            AND meter_update_timestamp < $end_time
+        WHERE meter_update_timestamp >= '{start_time}'
+            AND meter_update_timestamp < '{end_time}'
         GROUP BY bucket,
             cad_id
     )
@@ -60,4 +60,4 @@ COPY (
         AND power_data.cad_id = humidity_data.cad_id
     ORDER BY bucket,
         cad_id
-) TO 'chameleon_report.csv' WITH CSV HEADER;
+) TO '{output_file}' WITH CSV HEADER;
--- clients/chameleon/chameleon/report_generator.py
+++ clients/chameleon/chameleon/report_generator.py
@@ -1,15 +1,13 @@
 import duckdb
 
-db = duckdb.connect("chameleon.duckdb", read_only=True)
-
 with open("binning.sql") as f:
-    query = f.read()
-    db.execute(
-        query,
-        {
-            "start_time": "2025-01-01",
-            "end_time": "2027-01-02",
-        },
+    query = f.read().format(
+        start_time="2025-01-01",
+        end_time="2027-01-02",
+        interval="21 minutes",
+        output_file="chameleon_report.csv",
     )
 
-db.close()
+    db = duckdb.connect("chameleon.duckdb", read_only=True)
+    db.execute(query)
+    db.close()
